706d76d74496e252b1176a0daa410cca699192c6
Gustavo <gustavolopez819@gmail.com>
2026-09-01 15:42:24 -0500

    feat(touchpoints): add touchpoint icons

---
+++ src/features/branches/data/index.ts
@@ -0,0 +1,33 @@
+export const typesList = [
+  {
+    name: "Mesa",
+    value: "table",
+    icon: "/icons/picnic-table.svg",
+  },
+  {
+    name: "Barra",
+    value: "bar",
+    icon: "/icons/martini.svg",
+  },
+  {
+    name: "Entrada",
+    value: "entrance",
+    icon: "/icons/door-open.svg",
+  },
+  {
+    name: "Terraza",
+    value: "terrace",
+    icon: "/icons/stool.svg",
+  },
+  {
+    name: "Recepción",
+    value: "reception",
+    icon: "/icons/call-bell.svg",
+  },
+  {
+    name: "Mostrador",
+    value: "counter",
+    icon: "/icons/desk.svg",
+  },
+
+]
