2fe6d9a46050f2a42d2007ab4c5c2d53cfe1aa8a
Gustavo <gustavolopez819@gmail.com>
2026-09-04 23:23:40 -0500

    feat(hero): refine Hero component layout and styling for improved user experience

--- src/features/landing/components/Hero.tsx
+++ src/features/landing/components/Hero.tsx
@@ -1,19 +1,19 @@
 import { Button } from "@/components/ui/button";
-import Curve from "@/components/ui/curve";
 import { ChevronRight } from "lucide-react";
 import Link from "next/link";
+import CtaButton from "./cta-button";
 
 export default function Hero() {
 
   return (
     <div className="max-w-7xl mx-auto px-4 pt-20 lg:pt-32">
-      <div className="max-w-200 space-y-8">
+      <div className="max-w-180 space-y-8">
         <span className="flex items-center gap-3 font-lora">
-          <div className="w-1 h-3 rounded-full bg-primary"/>
+          <div className="w-1 h-3 rounded-full bg-primary" />
           Conexiones digitales para restaurantes
         </span>
-        <h1 className="text-[68px] text-6xl font-semibold text-[#1e2229]">
-         Una mesa. Un toque. Más formas de conectar.
+        <h1 className="text-[60px] text-6xl font-semibold text-charcoal">
+          Una mesa. Un toque. Más formas de conectar.
         </h1>
         <p className="text-xl text-muted-foreground">
           Acerca tu menú, tus promociones y tus redes sociales a cada mesa con etiquetas NFC. Gestiona tus enlaces desde Lennsi y descubre cómo interactúan tus clientes con tu restaurante.
@@ -21,19 +21,17 @@ export default function Hero() {
       </div>
 
       <div className="flex items-center gap-5 mt-10">
-        <Button
-          className='rounded-[12px] h-11'
-          size={'xl'}
-          variant={'chunk'}
-        >
-          Crear mi cuenta
-        </Button>
+        <CtaButton
+          link="/login"
+          className="bg-primary hover:bg-charcoal cursor-pointer"
+          text="Crear mi cuenta"
+        />
         <Link
-          href={'/demo'}
-          className="flex items-center gap-1.5 text-base font-bold text-[#1e2229] hover:text-[#1e2229]/80 transition-colors"
+          href={'/how-lennsi-works'}
+          className={'flex items-center gap-1 text-base font-bold text-charcoal transition-colors group'}
         >
           Cómo funciona
-          <ChevronRight />
+          <ChevronRight className="group-hover:translate-x-1.5 transition-transform" />
         </Link>
       </div>
     </div>
