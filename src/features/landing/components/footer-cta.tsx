b44788516dc59d9cf72f17a44bb2f210a930825f
Gustavo <gustavolopez819@gmail.com>
2026-09-07 14:19:35 -0500

    feat(footer): add responsive classes

--- src/features/landing/components/footer-cta.tsx
+++ src/features/landing/components/footer-cta.tsx
@@ -5,13 +5,13 @@ import * as motion from 'motion/react-client'
 export default function FooterCta() {
 
   return (
-    <section className="w-full h-130 relative z-20 rounded-t-[60px] bg-linear-to-tr from-charcoal to-charcoal/85">
+    <section className="w-full h-200 lg:h-130 relative z-20 rounded-t-[60px] bg-linear-to-tr from-charcoal to-charcoal/85">
       <div className="min-h-130 max-w-7xl grid lg:grid-cols-2 items-center gap-10 mx-auto px-6 pb-32 lg:px-8 lg:pb-40 pt-16  relative z-10">
         <div className="max-w-xl">
           <h2 className="text-4xl sm:text-5xl lg:text-5xl font-bold tracking-tight text-balance text-white">
             Todo lo que tu restaurante necesita, en un solo toque.
           </h2>
-          <div className="mt-8 flex flex-col gap-3 sm:flex-row">
+          <div className="mt-8 flex gap-3">
             <CtaButton
               link="/register"
               text="Comenzar ahora"
@@ -30,14 +30,14 @@ export default function FooterCta() {
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: .8, type: "spring", delay: 1 }}
 
-          className="relative hidden h-full lg:block"
+          className="relative h-full block"
         >
           <Image
             src="/img/mocks/cta-mockup.webp"
             alt="Plataforma Lennsi mostrada en un celular"
             width={650}
             height={900}
-            className="absolute left-1/2 top-1/2 w-100 max-w-none -translate-x-1/2 translate-y-[-33%] rotate-[5deg] object-contain"
+            className="absolute left-1/2 top-1/2 w-80 lg:w-100 max-w-none -translate-x-1/2 lg:translate-y-[-33%] rotate-[5deg] object-contain"
           />
         </motion.div>
       </div>
